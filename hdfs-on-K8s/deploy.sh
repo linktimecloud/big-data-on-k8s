73f5c38a577cc239750d2909f88494042e7b1ac5
vsong2016 <vincent@linktime.cloud>
2023-02-17 11:47:42 +0800

    simplify the dependency check code

--- hdfs-on-K8s/deploy.sh
+++ hdfs-on-K8s/deploy.sh
@@ -1,20 +1,5 @@
 set -e
 
-READY_STR=$(kubectl get pods|grep mysql |head -n 1|awk '{print $2}')
-while [ -z "${READY_STR}" ]; do
-  echo "waiting for MySQL to create"
-  sleep 5
-  READY_STR=$(kubectl get pods|grep mysql |head -n 1|awk '{print $2}')
-done
-
-arr=(${READY_STR//\// })
-while [ "${arr[0]}" != "${arr[1]}" ]; do
-  echo "waiting for MySQL to start"
-  sleep 5
-  READY_STR=$(kubectl get pods|grep mysql |head -n 1|awk '{print $2}')
-  arr=(${READY_STR//\// })
-done
-
 WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "WORK_DIR=${WORK_DIR}"
