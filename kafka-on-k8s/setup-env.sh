4b30f3947833398d759de6b67693cbf43ddc7290
vsong2016 <vincent@linktime.cloud>
2023-12-11 12:55:21 +0800

    change docker registry

--- kafka-on-k8s/setup-env.sh
+++ kafka-on-k8s/setup-env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export DOCKER_REGISTRY=ltc-registry.cn-hangzhou.cr.aliyuncs.com
+export DOCKER_REGISTRY=registry.cn-hangzhou.aliyuncs.com
 export DOCKER_ORG=linktimecloud
 export DOCKER_TAG=0.27.1
